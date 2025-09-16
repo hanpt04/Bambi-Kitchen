008ed6125786f4545b795689eeee6d8ffad03eb8
Thu ha <nha3697@gmail.com>
2025-09-16 12:57:07 +0700

    first commit

---
+++ BE/Bambi-Kitchen/src/main/java/gr1/fpt/bambikitchen/BambiKitchenApplication.java
@@ -0,0 +1,13 @@
+package gr1.fpt.bambikitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambiKitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BambiKitchenApplication.class, args);
+    }
+
+}
---
+++ BE/Bambi-Kitchen/src/test/java/gr1/fpt/bambikitchen/BambiKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package gr1.fpt.bambikitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambiKitchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
