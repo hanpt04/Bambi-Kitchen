ce83fbb180dad9f895626a69aa92c7d2eac4e177
Thu ha <nha3697@gmail.com>
2025-09-16 14:23:04 +0700

    test gitignore

---
+++ BE/src/main/java/gr1/fpt/bambikitchen/model/Account.java
@@ -0,0 +1,44 @@
+package gr1.fpt.bambikitchen.model;
+
+import gr1.fpt.bambikitchen.model.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Date;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "Accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank
+    private String name;
+    @NotNull(message = "Role must not be null")
+    private Role role;
+    @CreationTimestamp
+    private Date createAt;
+    @UpdateTimestamp
+    private Date updateAt;
+    private boolean isActive;
+    @NotBlank
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
+    private String password;
+    @NotBlank(message = "Mail must not be blank")
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
+            message = "Invalid email format"
+    )
+    private String mail;
+
+}
---
+++ BE/src/main/java/gr1/fpt/bambikitchen/model/enums/Role.java
@@ -0,0 +1,5 @@
+package gr1.fpt.bambikitchen.model.enums;
+
+public enum Role {
+ADMIN,STAFF,USER
+}
