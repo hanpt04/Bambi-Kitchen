01ce7eceb99abd774782ee744c3381d3854aa8fd
HoangTuGio <khangdeptrai198@gmail.com>
2025-09-17 16:37:04 +0700

    [BE][feat]: Integrate Swagger UI and configure database connection

---
+++ BE/src/main/java/gr1/fpt/bambikitchen/Config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package gr1.fpt.bambikitchen.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // định nghĩa Bearer JWT
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                // áp dụng cho toàn bộ API
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
