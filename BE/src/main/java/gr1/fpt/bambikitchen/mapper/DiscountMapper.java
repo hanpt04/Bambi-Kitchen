9184a6939dbc2d7782c8d1b6a3ef03096e588ab6
Chong <hungphucvp7@gmail.com>
2025-09-18 14:21:12 +0700

    [BE][feat]: Discount's CRUD

---
+++ BE/src/main/java/gr1/fpt/bambikitchen/mapper/DiscountMapper.java
@@ -0,0 +1,15 @@
+package gr1.fpt.bambikitchen.mapper;
+
+import gr1.fpt.bambikitchen.model.Discount;
+import gr1.fpt.bambikitchen.model.dto.request.DiscountCreateRequest;
+import gr1.fpt.bambikitchen.model.dto.request.DiscountUpdateRequest;
+import gr1.fpt.bambikitchen.service.DiscountService;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DiscountMapper {
+
+    Discount toDiscount(DiscountCreateRequest discount);
+
+    Discount toDiscount(DiscountUpdateRequest discount);
+}
