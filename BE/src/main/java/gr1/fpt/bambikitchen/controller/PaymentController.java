5c3b13c720d117ec3d2866dc8ab1fbe5ea08cc56
HoangTuGio <khangdeptrai198@gmail.com>
2025-09-18 16:40:48 +0700

    [Payment][feat]: Integrate VNPay payment gateway

--- BE/src/main/java/gr1/fpt/bambikitchen/controller/PaymentController.java
+++ BE/src/main/java/gr1/fpt/bambikitchen/controller/PaymentController.java
@@ -72,4 +72,62 @@ public class PaymentController {
         //o day redirect ve FE voi parameter de hien thi hoa don
         return ResponseEntity.ok(response);
     }
+
+
+    @GetMapping("/vnpay-return")
+    public String handleVnPayReturn(@RequestParam Map<String, String> params, HttpServletResponse httpResponse) throws Exception {
+        Map<String, Object> response = new HashMap<>();
+
+        // Lấy các thông tin quan trọng từ VNPay
+        String vnp_Amount = params.get("vnp_Amount");
+        String vnp_BankCode = params.get("vnp_BankCode");
+        String vnp_BankTranNo = params.get("vnp_BankTranNo");
+        String vnp_CardType = params.get("vnp_CardType");
+        String vnp_OrderInfo = params.get("vnp_OrderInfo");
+        String vnp_PayDate = params.get("vnp_PayDate");
+        String vnp_ResponseCode = params.get("vnp_ResponseCode");
+        String vnp_TransactionNo = params.get("vnp_TransactionNo");
+        String vnp_TransactionStatus = params.get("vnp_TransactionStatus");
+        String vnp_TxnRef = params.get("vnp_TxnRef");
+        String vnp_SecureHash = params.get("vnp_SecureHash");
+
+        // Kiểm tra mã phản hồi để xác định thanh toán có thành công hay không
+        boolean isSuccess = "00".equals(vnp_ResponseCode) && "00".equals(vnp_TransactionStatus);
+
+        // Tạo dữ liệu để gửi về frontend
+        response.put("orderId", vnp_TxnRef);
+        response.put("amount", Integer.parseInt(vnp_Amount) / 100);
+        response.put("bankCode", vnp_BankCode);
+        response.put("bankTransactionNo", vnp_BankTranNo);
+        response.put("cardType", vnp_CardType);
+        response.put("orderInfo", vnp_OrderInfo);
+        response.put("payDate", vnp_PayDate);
+        response.put("transactionNo", vnp_TransactionNo);
+        response.put("status", isSuccess ? "SUCCESS" : "FAILED");
+
+        // Lấy paymentId từ vnp_OrderInfo
+        String[] parts = vnp_OrderInfo.split(":");
+        String paymentId = parts[1];
+
+        System.out.println("paymentId: " + paymentId);
+
+        // Cập nhật trạng thái thanh toán vào database + gửi hoóa đơn vào mail + tạo notification + dissable url
+        if (isSuccess) {
+            System.out.println("thanh toan thanh cong VNPAY");
+
+        } else {
+            System.out.println("Thanh toan cancle");
+        }
+
+//        Payment payment = paymentService.getPaymentById(Integer.parseInt(paymentId));
+//        // Redirect về frontend với thông tin giao dịch qua query parameters
+//        String redirectUrl = "http://localhost:3000/order/status?"
+//                + "orderId=" + payment.getOrderId()
+//                + "&amount=" + (Integer.parseInt(vnp_Amount) / 100)
+//                + "&status=" + (isSuccess ? "SUCCESS" : "FAILED")
+//                + "&method=VNPAY";
+
+//        httpResponse.sendRedirect(redirectUrl);
+        return response.toString();
+    }
 }
