7770c32df35391dd1ae728ac4e0be05a4b4bb44d
Chong <hungphucvp7@gmail.com>
2025-09-25 13:15:46 +0700

    [BE][feat]: Reset password

---
+++ BE/src/main/java/gr1/fpt/bambikitchen/security/PasswordResetService.java
@@ -0,0 +1,34 @@
+package gr1.fpt.bambikitchen.security;
+
+import gr1.fpt.bambikitchen.repository.AccountRepository;
+import gr1.fpt.bambikitchen.security.OTP.OTPGeneratorService;
+import gr1.fpt.bambikitchen.security.OTP.OTPRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PasswordResetService {
+    private final OTPGeneratorService otpService;
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final OTPRepository otpRepository;
+
+    public boolean resetPassword(String otp, String email, String newPassword) {
+        if (!otpRepository.existsByOtpAndMailAndIsActiveOrderByCreatedAtDesc(otp, email, false)) {
+            return false;
+        }
+        log.info("Reset OTP for {}", email);
+
+        return accountRepository.findByMail(email)
+                .map(user -> {
+                    user.setPassword(passwordEncoder.encode(newPassword));
+                    accountRepository.save(user);
+                    return true;
+                })
+                .orElse(false);
+    }
+}
