b334ac177a1675aa901191e11af50a67dbe7498c
HoangTuGio <khangdeptrai198@gmail.com>
2025-10-11 15:33:16 +0700

    [Scheduler][SKELETON]: Add Scheduler component

---
+++ BE/src/main/java/gr1/fpt/bambikitchen/schedule/Scheduler.java
@@ -0,0 +1,16 @@
+package gr1.fpt.bambikitchen.schedule;
+
+import gr1.fpt.bambikitchen.model.InventoryOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+public class Scheduler  {
+
+
+}
