553c873cf35bf2d3bc177840e31ab072c03a6be2
Chong <hungphucvp7@gmail.com>
2025-10-21 08:27:55 +0700

    [UPDATE]: Cập nhật cho phần gửi order cho email của User (thành công)

--- BE/src/main/java/gr1/fpt/bambikitchen/repository/OrderDetailRepository.java
+++ BE/src/main/java/gr1/fpt/bambikitchen/repository/OrderDetailRepository.java
@@ -1,7 +1,11 @@
 package gr1.fpt.bambikitchen.repository;
 
 import gr1.fpt.bambikitchen.model.OrderDetail;
+import gr1.fpt.bambikitchen.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+    List<OrderDetail> findByOrders_Id(int orders_id);
 }
