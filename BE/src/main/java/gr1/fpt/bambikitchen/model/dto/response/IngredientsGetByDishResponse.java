e01bcd8d467ecfa1f65fb3104f3678d00601b0b3
Chong <hungphucvp7@gmail.com>
2025-11-05 12:29:35 +0700

    [FEATURE]: Refactor ingredient handling in dish response to include detailed ingredient information

--- BE/src/main/java/gr1/fpt/bambikitchen/model/dto/response/IngredientsGetByDishResponse.java
+++ BE/src/main/java/gr1/fpt/bambikitchen/model/dto/response/IngredientsGetByDishResponse.java
@@ -1,12 +1,15 @@
 package gr1.fpt.bambikitchen.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import gr1.fpt.bambikitchen.model.Account;
 import gr1.fpt.bambikitchen.model.Ingredient;
+import gr1.fpt.bambikitchen.model.IngredientCategory;
 import gr1.fpt.bambikitchen.model.enums.DishType;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
@@ -18,8 +21,19 @@ public class IngredientsGetByDishResponse {
     private String imageUrl;
     private String publicId;
     private Account account;
-    private List<Ingredient> ingredients;
+    private List<IngredientDetail> ingredients;
     private DishType dishType;
     private boolean isActive;
     private boolean isPublic;
+
+    @Builder
+    @Data
+    public static class IngredientDetail {
+        private int id;
+        private String name;
+        private Double storedQuantity;
+        private Integer neededQuantity;
+        private IngredientCategory category;
+        private String imageUrl;
+    }
 }
