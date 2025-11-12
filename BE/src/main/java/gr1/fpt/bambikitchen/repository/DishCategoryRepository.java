3a2c3d423e6604f01b5014668d834c80d1626002
Thu ha <nha3697@gmail.com>
2025-11-12 21:03:57 +0700

    [FEATURE]:xóa dish cate

--- BE/src/main/java/gr1/fpt/bambikitchen/repository/DishCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package gr1.fpt.bambikitchen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DishCategoryRepository extends JpaRepository<DishCategory,Integer> {
-    boolean existsByName(String name);
-}
